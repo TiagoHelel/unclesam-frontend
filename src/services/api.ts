c2a2a2bfba21afd2cda751318bc803ab0a0cf05b
tiagohelel <tiago_helel@hotmail.com>
2021-03-13 13:11:38 -0500

    Added email activation and first step of payment

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: 'https://api.tribeservices.com.br',
-  baseURL: "http://localhost:3333",
+  baseURL: "https://api.tribeservices.com.br",
 });
 
 export default api;
